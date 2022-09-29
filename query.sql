fe71357b633e0a67a395d5a3547d4f3ebf80fda7
ali 2236 <ali.gh2236@gmail.com>
2022-09-29 11:52:20 +0330

    q2 solved

--- query.sql
+++ query.sql
@@ -8,14 +8,15 @@ where (->stcot.finalday ?= stbday)
 && (->stcot.finalmonth ?= stbmonth);
 
 -- میخواهیم برای استاد و دانشجو های یک رشته که در یک روز و ماه متولد شده اند تولد بگیریم.نام هردو و تاریخ تولد را پیدا کنید.
-select stname, profs.profname as profname from (select stname, stbday, stbmonth, ->stdep->dep<-profdep<-prof as profs from student) where profs.profbday ?= stbday and profs.profbmonth ?= stbmonth;
+select st.stname as stname, prof.profname as profname, st.stbmonth as bmonth, st.stbday as bday 
+from (select id as st, ->stdep->dep<-profdep<-prof as prof 
+from student split prof)
+ where (prof.profbday == st.stbday) 
+ and (prof.profbmonth == st.stbmonth);
 
-select * from (select ->stdep->dep<-profdep<-prof from student);
-
-select name from (select stname as name, stbmonth as bmonth, stbday as bday from student), (select profname as name, profbmonth as bmonth, profbday as bday from prof) group by bmonth, bday;
-select stname, profname, bmonth, bday from (select stname, stbmonth as bmonth, stbday as bday from student), (select profname, profbmonth as bmonth, profbday as bday from prof) group by bmonth, bday; 
-select stname, pf.profname, st.stbmonth, st.stbday
- from STT st
- join PROFT pf on st.stdepid = pf.profdepid
-and st.stbmonth = pf.profbmonth
-and st.stbday = pf.profbday;
+-- سوال قبلی را برای دو دانشجو متولد یک روز و ماه که از یک رشته نباشند حل کنید.
+select st1.stname as st1name, st2.stname as st2name, st1.stbmonth as bmonth, st1.stbday as bday
+ from (select id as st1, (select id from student) as st2 from student split st2)
+  where (st1.stmjr != st2.stmjr) 
+  and (st1.stbmonth == st2.stbmonth) 
+  and (st1.stbday == st2.stbday);
